a873ae8a0e80473576bb09d3bfa1f271f32bd67c
Olugbenga19 <34916630+hulkyboi@users.noreply.github.com>
2020-10-02 10:02:03 +0100

    5

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,21 +2,18 @@ import { Component, OnInit } from "@angular/core";
 import { Product } from "../product";
 import { ProductService } from "../product.service";
 
-
-
 @Component({
   selector: "app-product-list",
   templateUrl: "./product-list.component.html",
   styleUrls: ["./product-list.component.css"],
-  
 })
 export class ProductListComponent implements OnInit {
-  constructor() {}
+  Products: Product[];
+  constructor(private _productService: ProductService) {}
 
   ngOnInit() {
-    this._productService.getProducts()
-      .subscribe(response => this.products = response);
+    this._productService
+      .getProducts()
+      .subscribe(response => this.Products = response);
   }
 }
-
-
